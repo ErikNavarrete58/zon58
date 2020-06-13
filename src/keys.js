424279f9c5aab2a1a64e55d82a7937c0d56e082a
ErikNavarrete58 <64390137+ErikNavarrete58@users.noreply.github.com>
2020-06-13 17:15:46 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
  database: {
-    host     : 'database-1.ccfivm33z2y3.us-east-2.rds.amazonaws.com',
-    user     : 'admin',
-    password : 'impacto2414',
-    database : 'futbolce_zon58'
+    host     : '',
+    user     : '',
+    password : '',
+    database : ''
 
  }
 };
